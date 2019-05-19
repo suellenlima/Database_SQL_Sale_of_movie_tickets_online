f7ec2887c17eb01ad9be343d4d92667365b810e1
SUPREME DEVELOPER <conradopsa@gmail.com>
2019-05-19 17:11:00 -0300

    Atendendo aos requisitos de consulta
    Requisitos
    Elaborar uma consulta com duas tabelas usando group by com funções de agregação
    Elaborar uma consulta com três tabelas usando group by com funções de agregação
    Elaborar uma consulta com subconsulta
    Elaborar uma StoredProcedure

--- DQL/DQL.sql
+++ DQL/DQL.sql
@@ -1,55 +1,36 @@
-/* Quantidade de poltronas por sessão */
-select se.id as 'idSessão', count(*)
-from sessao se join sala sa
-     on se.idSala = sa.idSala
-     join poltrona p
-     on p.idSala = sa.idSala
-group by se.id;
-
-/* Quantidade de poltronas por salas (exibindo os números) */
-select s.idSala, s.num, count(*)
-from sala s join poltrona p 
-     on s.idSala = p.idSala
-group by s.idSala;
-
-/* Quantidade de poltronas por salas (exibindo os números das salas e os nomes dos cinemas) */
-select s.idSala, c.nome, s.num, count(*)
-from sala s join poltrona p 
-     on s.idSala = p.idSala
-     join cinema c
-     on c.cnpj = s.cnpjCinema
-group by s.idSala;
-
-/* Valor máximo, minimo e total de ingressos comprados */
+/* Valor máximo, minimo e valor total de ingressos comprados */
 select MAX(i.valor) as 'Valor Maximo Comprado', MIN(i.valor) as 'Valor Minimo Comprado', sum(i.valor) as 'Valor total'
 from ingresso i join ingresso_comprado ic
 	 on ic.codigoIngresso = i.codigo
-group by 'Valor', 'Valor Minimo Comprado', 'Valor total'
+group by 'Valor', 'Valor Minimo Comprado', 'Valor total';
 
-
-/* Informar nome do cliente, codigo do ingresso, id sessao, data e hora do ingresso comprado */
-select count(*) as 'q', c.nomeCompleto as 'Nome', ic.codigoIngresso as 'Codigo Ingresso', i.idSessao as 'Sessao', ic.DataHora as 'Data e Hora'
-from cliente c join ingresso_comprado ic
-	 on c.cpf = ic.cpfCliente
-     join ingresso i 
+/* Quantidade de ingressos comprados para cada sessão, agrupados por idsessão */
+select s.id, count(*) as 'Ingressos comprados'
+from ingresso_comprado ic join ingresso i
      on ic.codigoIngresso = i.codigo
-group by c.nomecompleto
+     join sessao s 
+     on s.id = i.idSessao
+group by s.id;
 
-
-/* Qtd de clientes que não irão assistir ao filme dos Vingadores com subconsulta */
-
-select count(*) as 'Quantidade de clientes que não irão assistir Os Vingadores'
+/* Quantidade de ingressos comprados que não são do filme Os Vingadores com subconsulta */
+select count(*) as 'Quantidade de ingressos comprados que não são do filme Os Vingadores'
 from ingresso_comprado ic join ingresso i
 	 on ic.codigoIngresso = i.codigo
      join sessao s on s.id = i.idsessao
      join filme f on f.id = s.idfilme
-where f.titulo <> 
-
+where f.titulo not in  
 (select f.titulo
 from ingresso_comprado ic join ingresso i
 	 on ic.codigoIngresso = i.codigo
      join sessao s on s.id = i.idsessao
      join filme f on f.id = s.idfilme
-where f.titulo = 'Os Vingadores')
-     
-     
+where f.titulo = 'Os Vingadores');
+
+/* Nome dos clientes com StoredProcedure */
+Delimiter $$
+create procedure nomeClientes()
+begin
+	select nomeCompleto
+    from cliente;
+end $$
+delimiter ;
